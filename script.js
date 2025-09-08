143bc8bd0323c16305f33dd1e86b29b5f6d738c4
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-09-08 17:52:32 -0300

    Add: Funcinalidad en Js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
-const apiKey = '2da6bd37813b8d2e290a62ee872e7ceb';
+const apiKey = 'API-KEY';
 const diffKelvinCelsius = 273.15;
 
 document.getElementById('searchButton').addEventListener('click', () => {
@@ -14,5 +14,42 @@ document.getElementById('searchButton').addEventListener('click', () => {
 function fetchWeather(city) {
     fetch(`${urlBase}?q=${city}&appid=${apiKey}&lang=es`)
     .then(data => data.json())
-    .then(data => console.log(data))
+    .then(data => showWeatherData(data))
+}
+
+function showWeatherData(data){
+    const divResponseData = document.getElementById('responseData');
+    divResponseData.innerHTML = '';
+
+    const cityName = data.name;
+    const countryName = data.sys.country;
+    const temp = data.main.temp;
+    const humidity = data.main.humidity;
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon
+
+    const cityInfo = document.createElement('h2');
+    cityInfo.textContent = `${cityName}, ${countryName}`;
+
+    const tempInfor = document.createElement('p');
+    tempInfor.textContent = `Temperatura: ${ (temp - diffKelvinCelsius).toFixed(2) } °C`;
+
+    const humidityInfo = document.createElement('p');
+    humidityInfo.textContent = `Humedad: ${humidity} %`;
+
+    const iconInfo = document.createElement('img');
+    iconInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+    const descriptionInfo = document.createElement('p');
+    descriptionInfo.textContent = `Descripción: ${capitalizar1eraLetra(description)}`;
+
+    divResponseData.appendChild(cityInfo);
+    divResponseData.appendChild(tempInfor);
+    divResponseData.appendChild(humidityInfo);
+    divResponseData.appendChild(iconInfo);
+    divResponseData.appendChild(descriptionInfo);
+}
+
+function capitalizar1eraLetra(texto) {
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
 }
